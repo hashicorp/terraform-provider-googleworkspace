7cd444feddb15200768aa7a271dd88e143ef1aad
Megan Bang <mbang@hashicorp.com>
2021-03-10 12:24:11 -0600

    update Scaffolding to Google Workspace

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
+	"github.com/hashicorp/terraform-provider-googleworkspace/internal/provider"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
@@ -37,8 +37,7 @@ func main() {
 	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
 
 	if debugMode {
-		// TODO: update this string with the full name of your provider as used in your configs
-		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/scaffolding", opts)
+		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/googleworkspace", opts)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
