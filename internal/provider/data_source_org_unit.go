f4406675bccbb37c934070870d09c9808c365bb5
Megan Bang <mbang@hashicorp.com>
2021-05-26 16:28:34 -0500

    add ability go get org unit datasource with org_unit_path

--- internal/provider/data_source_org_unit.go
+++ internal/provider/data_source_org_unit.go
@@ -2,14 +2,16 @@ package googleworkspace
 
 import (
 	"context"
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"strings"
 )
 
 func dataSourceOrgUnit() *schema.Resource {
 	// Generate datasource schema from resource
 	dsSchema := datasourceSchemaFromResourceSchema(resourceOrgUnit().Schema)
-	addRequiredFieldsToSchema(dsSchema, "org_unit_id")
+	addExactlyOneOfFieldsToSchema(dsSchema, "org_unit_id", "org_unit_path")
 
 	return &schema.Resource{
 		// This description is used by the documentation generator and the language server.
@@ -24,6 +26,40 @@ func dataSourceOrgUnit() *schema.Resource {
 func dataSourceOrgUnitRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	if d.Get("org_unit_id") != "" {
 		d.SetId(d.Get("org_unit_id").(string))
+	} else {
+		var diags diag.Diagnostics
+
+		// use the meta value to retrieve your client from the provider configure method
+		client := meta.(*apiClient)
+
+		directoryService, diags := client.NewDirectoryService()
+		if diags.HasError() {
+			return diags
+		}
+
+		orgUnitsService, diags := GetOrgUnitsService(directoryService)
+		if diags.HasError() {
+			return diags
+		}
+
+		orgUnitPath := d.Get("org_unit_path").(string)
+		ouPath := strings.TrimLeft(orgUnitPath, "/")
+
+		orgUnit, err := orgUnitsService.Get(client.Customer, ouPath).Do()
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		if orgUnit == nil {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("No org unit was returned for %s.", orgUnitPath),
+			})
+
+			return diags
+		}
+
+		d.SetId(orgUnit.OrgUnitId)
 	}
 
 	return resourceOrgUnitRead(ctx, d, meta)
--- internal/provider/data_source_org_unit_test.go
+++ internal/provider/data_source_org_unit_test.go
@@ -28,6 +28,26 @@ func TestAccDataSourceOrgUnit_withOrgUnitId(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceOrgUnit_withOrgUnitPath(t *testing.T) {
+	t.Parallel()
+
+	ouName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceOrgUnit_withOrgUnitPath(ouName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.googleworkspace_org_unit.my-new-org-unit", "name", ouName),
+				),
+			},
+		},
+	})
+}
+
 func testAccDataSourceOrgUnit_withOrgUnitId(ouName string) string {
 	return fmt.Sprintf(`
 resource "googleworkspace_org_unit" "my-new-org-unit" {
@@ -37,6 +57,19 @@ resource "googleworkspace_org_unit" "my-new-org-unit" {
 
 data "googleworkspace_org_unit" "my-new-org-unit" {
   org_unit_id = googleworkspace_org_unit.my-new-org-unit.id
+}
+`, ouName)
+}
+
+func testAccDataSourceOrgUnit_withOrgUnitPath(ouName string) string {
+	return fmt.Sprintf(`
+resource "googleworkspace_org_unit" "my-new-org-unit" {
+  name = "%s"
+  parent_org_unit_path = "/"
+}
+
+data "googleworkspace_org_unit" "my-new-org-unit" {
+  org_unit_path = googleworkspace_org_unit.my-new-org-unit.org_unit_path
 }
 `, ouName)
 }
