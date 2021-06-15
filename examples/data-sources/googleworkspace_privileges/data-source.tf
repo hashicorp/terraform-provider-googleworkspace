b57c4bad823e9f5b0955e4ffe19ff033d08d54b8
Alex Pilon <apilon@hashicorp.com>
2021-06-15 18:37:26 -0400

    Fix typo

--- examples/data-sources/googleworkspace_privileges/data-source.tf
+++ examples/data-sources/googleworkspace_privileges/data-source.tf
@@ -20,7 +20,7 @@ resource "googleworkspace_role" "read-only" {
   name = "read-only"
 
   dynamic "privileges" {
-    for_each = locals.read_only_privileges
+    for_each = local.read_only_privileges
     content {
       service_id     = privileges.value["service_id"]
       privilege_name = privileges.value["privilege_name"]
