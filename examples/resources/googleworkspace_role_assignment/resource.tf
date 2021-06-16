058750408ded808f0c55cd3de750cfe10585337e
Alex Pilon <apilon@hashicorp.com>
2021-06-15 20:46:27 -0400

    Enable org_unit scoping

--- examples/resources/googleworkspace_role_assignment/resource.tf
+++ examples/resources/googleworkspace_role_assignment/resource.tf
@@ -17,3 +17,38 @@ resource "googleworkspace_role_assignment" "dwight-ra" {
   role_id     = data.googleworkspace_role.groups-admin.id
   assigned_to = googleworkspace_user.dwight.id
 }
+
+# role assignments can be scoped to org units, however all privileges must be org_unit compatible
+
+data "googleworkspace_privileges" "privileges" {}
+
+locals {
+  org_scopable_privileges = [
+    for priv in data.googleworkspace_privileges.privileges.items : priv
+    if priv.is_org_unit_scopable
+  ]
+}
+
+resource "googleworkspace_role" "org-unit-privs" {
+  name = "org-unit-privs"
+
+  dynamic "privileges" {
+    for_each = local.org_scopable_privileges
+    content {
+      service_id     = privileges.value["service_id"]
+      privilege_name = privileges.value["privilege_name"]
+    }
+  }
+}
+
+resource "googleworkspace_org_unit" "org-unit" {
+  name                 = "org-unit"
+  parent_org_unit_path = "/"
+}
+
+resource "googleworkspace_role_assignment" "org-unit-scope" {
+  role_id     = googleworkspace_role.org-unit-privs.id
+  assigned_to = googleworkspace_user.dwight.id
+  scope_type  = "ORG_UNIT"
+  org_unit_id = googleworkspace_user.org-unit.id
+}
