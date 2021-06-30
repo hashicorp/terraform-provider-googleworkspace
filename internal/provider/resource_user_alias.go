e3684291dcf6bc9cbfedfa57dd3bd0e9c1d145e3
Thomas Goodsell <thomas.goodsell@tempus.com>
2021-06-30 16:10:26 -0500

    Revert "remove etag since that's not a thing for aliases"
    This reverts commit b29d29b4da3091ff785b7aa79dbfa9b9390a00ff.

--- internal/provider/resource_user_alias.go
+++ internal/provider/resource_user_alias.go
@@ -41,6 +41,11 @@ func resourceUserAlias() *schema.Resource {
 				Required:    true,
 				ForceNew:    true,
 			},
+			"etag": {
+				Description: "ETag of the resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
 		},
 	}
 }
@@ -97,6 +102,7 @@ func resourceUserAliasCreate(ctx context.Context, d *schema.ResourceData, meta i
 	d.SetId(fmt.Sprintf("%s/%s", alias.PrimaryEmail, alias.Alias))
 	d.Set("primary_email", alias.PrimaryEmail)
 	d.Set("alias", alias.Alias)
+	d.Set("etag", alias.Etag)
 	return resourceUserAliasRead(ctx, d, meta)
 }
 
@@ -137,6 +143,7 @@ func resourceUserAliasRead(ctx context.Context, d *schema.ResourceData, meta int
 	d.SetId(fmt.Sprintf("%s/%s", alias.PrimaryEmail, alias.Alias))
 	d.Set("primary_email", alias.PrimaryEmail)
 	d.Set("alias", alias.Alias)
+	d.Set("etag", alias.Etag)
 	return nil
 }
 
@@ -218,9 +225,10 @@ func doesAliasExist(aliasesResp *admin.Aliases, expectedAlias string) (*admin.Al
 			if expectedAlias == safeInterfaceToString(alias["alias"]) {
 				return &admin.Alias{
 					Alias:        safeInterfaceToString(alias["alias"]),
+					Etag:         safeInterfaceToString(alias["etag"]),
 					Id:           safeInterfaceToString(alias["id"]),
 					Kind:         safeInterfaceToString(alias["kind"]),
-					PrimaryEmail: safeInterfaceToString(alias["primary_email"]),
+					PrimaryEmail: safeInterfaceToString(alias["primaryemail"]),
 				}, true
 			}
 		}
