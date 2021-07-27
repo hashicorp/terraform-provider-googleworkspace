f9ba7aad6cea350387a3abcff88922548830166c
megan07 <mbang@hashicorp.com>
2021-07-27 14:22:58 -0500

    update role assignment (#124)

--- .github/infra/workspace.tf
+++ .github/infra/workspace.tf
@@ -5,7 +5,11 @@ data "googleworkspace_role" "groups-admin" {
   name = "_GROUPS_ADMIN_ROLE"
 }
 
+data "google_service_account" "vault-sa" {
+  account_id = vault_gcp_secret_roleset.roleset.service_account_email
+}
+
 resource "googleworkspace_role_assignment" "sa-groups-admin" {
   role_id = data.googleworkspace_role.groups-admin.id
-  assigned_to = vault_gcp_secret_roleset.roleset.service_account_email
+  assigned_to = data.google_service_account.vault-sa.unique_id
 }
