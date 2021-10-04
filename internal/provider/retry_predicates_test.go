180e73abfe927d02740158be1a016b1cce08decb
Megan Bang <mbang@hashicorp.com>
2021-10-04 14:07:37 -0500

    remove 429 redundancy

--- internal/provider/retry_predicates_test.go
+++ internal/provider/retry_predicates_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestIsCommonRetryableErrorCode_retryableErrorCode(t *testing.T) {
-	codes := []int{429, 500, 502, 503}
+	codes := []int{500, 502, 503}
 	for _, code := range codes {
 		code := code
 		t.Run(strconv.Itoa(code), func(t *testing.T) {
@@ -39,6 +39,17 @@ func TestIsOperationReadQuotaError_quotaExceeded(t *testing.T) {
 	err := googleapi.Error{
 		Code: 403,
 		Body: "Request rate higher than configured., quotaExceeded",
+	}
+	isRetryable, _ := isRateLimitExceeded(&err)
+	if !isRetryable {
+		t.Errorf("Error not detected as retryable")
+	}
+}
+
+func TestIsOperationReadQuotaError_rateLimitExceeded(t *testing.T) {
+	err := googleapi.Error{
+		Code: 429,
+		Body: "Rate Limit Exceeded., rateLimitExceeded",
 	}
 	isRetryable, _ := isRateLimitExceeded(&err)
 	if !isRetryable {
