6ff91f3dd5b6b5b9151427f7a62e90b11bb5f949
Megan Bang <mbang@hashicorp.com>
2021-10-11 10:54:39 -0500

    add group_members resource back

---
+++ internal/provider/data_source_group_members.go
@@ -0,0 +1,28 @@
+package googleworkspace
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGroupMembers() *schema.Resource {
+	// Generate datasource schema from resource
+	dsSchema := datasourceSchemaFromResourceSchema(resourceGroupMembers().Schema)
+	addRequiredFieldsToSchema(dsSchema, "group_id")
+
+	return &schema.Resource{
+		// This description is used by the documentation generator and the language server.
+		Description: "Group Members data source in the Terraform Googleworkspace provider. Group Members resides " +
+			"under the `https://www.googleapis.com/auth/admin.directory.group` client scope.",
+
+		ReadContext: dataSourceGroupMembersRead,
+
+		Schema: dsSchema,
+	}
+}
+
+func dataSourceGroupMembersRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return resourceGroupMembersRead(ctx, d, meta)
+}
